4f6510affce3d47a82ef7a4099b5640ef9b48dfd
Christian <ps5christian@hotmail.com>
2024-01-24 18:42:19 +0100

    Made the modal header

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -10,9 +10,12 @@ const modalWindow = document.createElement("div");
 modalWindow.classList.add("modal-window");
 
 const modalHeader = document.createElement("header");
+modalHeader.classList.add("modal-header");
 const modalHeaderText = document.createElement("h2");
+modalHeaderText.textContent = "Welcome";
 
 const modalHeaderBtnContainer = document.createElement("span");
+modalHeaderBtnContainer.classList.add("x-btn-container");
 const modalHeaderBtn = document.createElement("a");
 modalHeaderBtn.innerHTML = "&times;";
 modalHeaderBtnContainer.appendChild(modalHeaderBtn);
